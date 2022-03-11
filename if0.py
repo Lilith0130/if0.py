2d0f5e454b8128fb95db126c8f5cbb8aa0a30c6d
LILITHl <Saosao007888@gmail.com>
2022-03-11 18:29:34 +0800

    add new file

---
+++ if0.py
@@ -0,0 +1,16 @@
+rain = input('請問今天有沒有下雨:')
+
+if rain == '有' :
+	print('撐傘出門')
+	print('買包洋芋片')
+	print('在家看電影')
+	if x > 3:
+		print('x大於三')
+
+if rain == '沒有' :
+	print('出門喝咖啡')
+	print('順便逛誠品')
+	print('go_flex看帥哥')
+	print('過著精彩的一天')
+	print('或者')
+	print('在家耍廢')
